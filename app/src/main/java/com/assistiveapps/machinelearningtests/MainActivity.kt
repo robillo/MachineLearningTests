56cec25fdd3e1cd83c51e10b4bf99802b83c842b
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-09-18 04:48:14 +0530

    base ui for main screen

--- app/src/main/java/com/assistiveapps/machinelearningtests/MainActivity.kt
+++ app/src/main/java/com/assistiveapps/machinelearningtests/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.assistiveapps.machinelearningtests
 
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeStatusBarColor()
+        initialize()
+        setClickListeners()
+    }
+
+    fun setClickListeners() {
+
+    }
+
+    fun initialize() {
+
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase))
+    }
+
+    fun changeStatusBarColor() {
+        val window = window ?: return
+        val view = window.decorView ?: return
+        var flags = view.systemUiVisibility
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        view.systemUiVisibility = flags
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
     }
 }
